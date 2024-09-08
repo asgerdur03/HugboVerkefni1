62f12d08e0c8f7cefa5d7909c82282ee0a56190a
hemmih <hermannih0@gmail.com>
2024-09-08 14:36:38 +0000

    init commit2

--- src/main/java/hi/hugboverkefni1/HugboVerkefni1Application.java
+++ src/main/java/hi/hugboverkefni1/HugboVerkefni1Application.java
@@ -2,7 +2,7 @@ package hi.hugboverkefni1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Hermann Ingi - hih65@hi.is
 @SpringBootApplication
 public class HugboVerkefni1Application {
 
