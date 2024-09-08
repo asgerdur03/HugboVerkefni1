d36c233ab9f7bcc04fd42d610273db4faa5c0d0f
hemmih <hermannih0@gmail.com>
2024-09-08 14:32:49 +0000

    laga branchves

--- src/main/java/hi/hugboverkefni1/HugboVerkefni1Application.java
+++ src/main/java/hi/hugboverkefni1/HugboVerkefni1Application.java
@@ -2,11 +2,6 @@ package hi.hugboverkefni1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//Hermann Ingi - hih65@hi.is
-//Freydís Xuan - fxl1hi.is
-//Ásgerður Júlía - ajg20@hi.is
-//Vilborg Erlendsdóttir - vie15@hi.is
-
 
 @SpringBootApplication
 public class HugboVerkefni1Application {
