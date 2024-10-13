e5779fc2e98495e96452f2b1d96769ce350922d5
asgerdur03 <asgerdur03@gmail.com>
2024-10-13 20:17:17 +0000

    jpa sett upp, en á eftir að tengja saman tables. signup komið, ekki login samt

--- src/main/java/hi/hugboverkefni1/Controllers/UserController.java
+++ src/main/java/hi/hugboverkefni1/Controllers/UserController.java
@@ -1,12 +1,19 @@
 package hi.hugboverkefni1.Controllers;
 
 
+import hi.hugboverkefni1.persistence.entities.Task;
 import hi.hugboverkefni1.persistence.entities.User;
 import hi.hugboverkefni1.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 @Controller
 public class UserController {
@@ -19,18 +26,42 @@ public class UserController {
     }
 
 
+    // Wep opening page, opens to login and signup screen
     @RequestMapping("/")
-    public String UserController() {
-        //busines logic
-        //call a methodd in a service class
-        // add some data to the model
-        return "opening-page"; //leitar að html file clalled opening-page
+    public String home(Model model) {
+        List<User> allUsers = userService.getUsers();
+        model.addAttribute("users", allUsers);
+        return "opening-page";
     }
 
     @RequestMapping("/signup")
-    public String signupPage() {
+    public String signup(Model model) {
+        model.addAttribute("user", new User());
         return "signup";
     }
+
+
+    // adds user to database?
+    @RequestMapping(value = "/signup", method = RequestMethod.POST)
+    public String signup(User user, BindingResult result, Model model) {
+        if(result.hasErrors()) {
+            return "opening-page";
+        }
+        userService.saveUser(user);
+        return "redirect:/";
+    }
+
+    @RequestMapping(value="/delete/{id}")
+    public String deleteUser(@PathVariable("id") long id, Model model) {
+        User userToDelete = userService.findUserById(id);
+        userService.deleteUser(userToDelete);
+        return "redirect:/";
+    }
+
+
+
+
+
 
 
 
