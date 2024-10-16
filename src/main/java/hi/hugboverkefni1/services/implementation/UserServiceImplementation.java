4568742e7a181fa9d5bfa0fc1ef0831f3851659d
Vilborg Erlendsdóttir <vie15@hi.is>
2024-10-16 22:23:17 +0000

    Login virkar en ekki login error

--- src/main/java/hi/hugboverkefni1/services/implementation/UserServiceImplementation.java
+++ src/main/java/hi/hugboverkefni1/services/implementation/UserServiceImplementation.java
@@ -4,10 +4,8 @@ import hi.hugboverkefni1.persistence.entities.User;
 import hi.hugboverkefni1.persistence.respositories.UserRepository;
 import hi.hugboverkefni1.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,6 +21,11 @@ public class UserServiceImplementation implements UserService {
     @Override
     public List<User> getUsers() {
         return userRepository.findAll();
+    }
+
+    @Override
+    public User findUsername(String username) {
+        return userRepository.findByUsername(username).orElse(null);
     }
 
     @Override
@@ -42,11 +45,12 @@ public class UserServiceImplementation implements UserService {
         userRepository.delete(user);
 
     }
-
+/*
     @Override
     public User findUsername(String username) {
         return userRepository.findByUsername(username);
-    }
+    } */
+    /*
 
     @Override
     public User validateUser(String username, String password) {
@@ -56,9 +60,33 @@ public class UserServiceImplementation implements UserService {
             return user;
         }
         return null;
+    }*/
+
+    /* validate user boolean í vinnslu'/
+
+     */ /*
+    public boolean validateUser(String username, String password){
+        return username.equalsIgnoreCase("admin") && password.equalsIgnoreCase("password");
+    }  Version 2 , virkar ekki*/
+
+    @Override
+    public boolean validateUser(String username, String password) {
+        Optional<User> user = userRepository.findByUsername(username);
+
+        if (user.isEmpty()) {
+            System.out.println("User not found: " + username);
+            return false;
+        }
+
+
+        if (user.get().getPassword().equals(password)) {
+            System.out.println("User validated successfully: " + username);
+            return true;
+        } else {
+            System.out.println("Invalid password for user: " + username);
+            return false;
+        }
     }
-
-
 
 
     /*
